28024292720543ccccd986e299d4a27f4a25ca70
johnnyman727 <johnnyman727@gmail.com>
2014-10-24 16:47:56 -0700

    Completes readme

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
 rgb-tcs34725
 ============
 
-A JavaScript driver for the TCS34725 RGB sensor
+A JavaScript driver for the TCS34725 RGB sensor. Use it to find the red, green, blue, or clear levels of anything you point it at. The API gives you access to the raw RGBC data, color temperatures, and lux intensity.
+
+The chip also has support for threshold interrupts on the clear channel and those APIs have been implemented here but not fully tested.
+
+## Installation
+```npm install rgb-tcs34725```
+
+## Example Usage
+```.js
+var tessel = require('tessel');
+var rgbLib = require('../');
+var rgb = rgbLib.use(tessel.port.A);
+
+rgb.on('ready', function() {
+  
+  setInterval(function() {
+    rgb.getRawData(function(err, colors) {
+      if (err) throw err;
+
+      console.log('RED:', colors.red);
+      console.log('GREEN:', colors.green);
+      console.log('BLUE:', colors.blue);
+      console.log('CLEAR:', colors.clear);
+    })
+  }, 1000);
+});
+```
+
+## API
+
+### Commands
+
+```library.use(hardware, callback)``` initializes the module on the port of a microcontroller. It returns the `rgb` object and the callback has the form of `callback(err, rgb)`.
+
+```rgb.getRawData(err, colors)``` returns rgbc data. `colors` has `red`, `green`, `blue`, and `clear` properties. Each of the colors is a 16 bit number.
+
+```rgb.calculateColorTemperature(callback(err, temp))``` returns the color temperature as a 16 bit number representing Kelvins. The temperature it returns is in units of Kelvin.
+
+```rgb.calculateLux(callback(err, lux))``` returns the light intensity as a 16 bit number that represents the light intensity.
+
+```rgb.enable(callback(err))``` enables the module to read colors. It is called automatically when the `use` function is called.
+
+```rgb.disable()``` can be used to put the module to sleep and reduce power consumption. Call ```rgb.enable()``` to wake it back up.
+
+```rgb.setIntegrationTime(time, callback)``` sets the amount of time integrating values from an ADC (which determines color). Longer times produce more accurate numbers at low light levels. Available times can be found with `rgb.integrationTimes`.
+
+```rgb.setGain(gain, callback)``` sets the gain of the ADC. Available gains can be found with `rgb.gains`.
--- index.js
+++ index.js
@@ -10,27 +10,21 @@ var TCS34725_COMMAND_BIT = 0x80; // The command to signal that we're command it 
 var TCS34725_ENABLE = 0x00; // Enable operations
 var TCS34725_ENABLE_PON = 0x01; // Power on
 var TCS34725_ENABLE_AEN = 0x02; // RGBC Enable
+var TCS34725_ENABLE_AIEN = 0x10; /* RGBC Interrupt Enable */
 
 var TCS34725_ATIME = 0x01; // Amount of time for ADC integration
 
 var TCS34725_CONTROL = 0x0F; // Set the gain level
 
-var TCS34725_INTEGRATIONTIME_2_4MS  = 0xFF;   /**<  2.4ms - 1 cycle    - Max Count: 1024  */
-var TCS34725_INTEGRATIONTIME_24MS   = 0xF6;   /**<  24ms  - 10 cycles  - Max Count: 10240 */
-var TCS34725_INTEGRATIONTIME_50MS   = 0xEB;   /**<  50ms  - 20 cycles  - Max Count: 20480 */
-var TCS34725_INTEGRATIONTIME_101MS  = 0xD5;   /**<  101ms - 42 cycles  - Max Count: 43008 */
-var TCS34725_INTEGRATIONTIME_154MS  = 0xC0;   /**<  154ms - 64 cycles  - Max Count: 65535 */
-var TCS34725_INTEGRATIONTIME_700MS  = 0x00;   /**<  700ms - 256 cycles - Max Count: 65535 */
-
-var TCS34725_GAIN_1X                = 0x00;   /**<  No gain  */
-var TCS34725_GAIN_4X                = 0x01;   /**<  2x gain  */
-var TCS34725_GAIN_16X               = 0x02;   /**<  16x gain */
-var TCS34725_GAIN_60X               = 0x03;   /**<  60x gain */
-
 var TCS34725_CDATAL = 0x14; // Clear channel data
 var TCS34725_RDATAL = 0x16; // Red channel data
 var TCS34725_GDATAL = 0x18; // Green channel data
 var TCS34725_BDATAL = 0x1A; // Blue channel data
+
+var TCS34725_AILTL = 0x04 // Clear Low channel interrupt level
+var TCS34725_AILTH = 0x05 // Set low channel interrupt level
+var TCS34725_AIHTL = 0x06 // Clear high channel interrupt level
+var TCS34725_AIHTH = 0x07 // Set high channel interrupt level
 
 function use(hardware, callback) {
   return new RGB(hardware, callback);
@@ -41,6 +35,8 @@ function RGB(hardware, callback) {
   var self = this;
   self.hardware = hardware;
   self.i2c = hardware.I2C(TCS34725_ADDRESS);
+  self.irq = hardware.digital[0];
+  self.led = hardware.digital[1];
   self._initialize(function(err) {
     if (self.failCallback(err, callback)) {
       setImmediate(function() {
@@ -62,6 +58,22 @@ function RGB(hardware, callback) {
 
 util.inherits(RGB, events.EventEmitter);
 
+RGB.prototype.integrationTimes = {
+  2.4 : 0xFF, /**<  2.4ms - 1 cycle    - Max Count: 1024  */
+  24 : 0xF6, /**<  24ms  - 10 cycles  - Max Count: 10240 */
+  50 : 0xEB, /**<  50ms  - 20 cycles  - Max Count: 20480 */
+  101 : 0xD5, /**<  101ms - 42 cycles  - Max Count: 43008 */
+  154 : 0xC0, /**<  154ms - 64 cycles  - Max Count: 65535 */
+  300 : 0x00, /**<  700ms - 256 cycles - Max Count: 65535 */
+};
+
+RGB.prototype.gains = {
+  1 : 0x00, /**<  No gain  */
+  4 : 0x01, /**<  2x gain  */
+  16 : 0x02, /**<  16x gain */
+  60 : 0x03, /**<  60x gain */
+};
+
 RGB.prototype.failCallback = function(err, callback) {
   if (err) {
 
@@ -73,7 +85,7 @@ RGB.prototype.failCallback = function(err, callback) {
   }
 
   return false;
-}
+};
 
 RGB.prototype._initialize = function(callback) {
   var self = this;
@@ -84,20 +96,20 @@ RGB.prototype._initialize = function(callback) {
       return self.failCallback(err, callback);
     }
     else {
-      self.setIntegrationTime(TCS34725_INTEGRATIONTIME_700MS, function(err) {
+      self.setIntegrationTime(self.integrationTimes[24], function(err) {
         if (!self.failCallback(err, callback)) {
-          self.setGain(TCS34725_GAIN_1X, function(err) {
+          self.setGain(self.gains[1], function(err) {
             if (!self.failCallback(err, callback)) {
-              self._enable(callback);
+              self.enable(callback);
             }
           });
         }
       });
     }
   });
-}
+};
 
-RGB.prototype._enable = function(callback) {
+RGB.prototype.enable = function(callback) {
   var self = this;
 
   function fail(err, callback) {
@@ -121,30 +133,38 @@ RGB.prototype._enable = function(callback) {
       })
     }
   })
-}
+};
+
+RGB.prototype.disable = function(callback) {
+  reg & ~(TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN)
+  self._read8Bits(TCS34725_ENABLE, function(err, value) {
+    value = valuereadUInt8(0);
+    value &= ~(TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN);
+    self._write8Bits(value, callback);
+  })
+};
 
 RGB.prototype.setIntegrationTime = function(time, callback) {
   this._write8Bits(TCS34725_ATIME, time, callback);
-}
+};
 
 RGB.prototype.setGain = function(gain, callback) {
   this._write8Bits(TCS34725_CONTROL, gain, callback);
-}
+};
 
 RGB.prototype._read8Bits = function(reg, callback) {
   this.i2c.transfer(new Buffer([TCS34725_COMMAND_BIT | reg]), 1, callback);
-}
+};
 
 RGB.prototype._read16Bits = function(reg, callback) {
   this.i2c.transfer(new Buffer([TCS34725_COMMAND_BIT | reg]), 2, callback);
-}
+};
 
 RGB.prototype._write8Bits = function(reg, value, callback) {
-  console.log('writing', parseInt(TCS34725_COMMAND_BIT | reg).toString(16), parseInt(value & 0xFF).toString());
   this.i2c.send(new Buffer([TCS34725_COMMAND_BIT | reg, value & 0xFF]), callback);
-}
+};
 
-RGB.prototype.getRGBC = function(callback) {
+RGB.prototype.getRawData = function(callback) {
   var self = this;
 
   var rgbc = {};
@@ -169,7 +189,7 @@ RGB.prototype.getRGBC = function(callback) {
       }
     }
   });
-}
+};
 
 RGB.prototype.readColor = function(color, callback) {
   var self = this;
@@ -192,8 +212,6 @@ RGB.prototype.readColor = function(color, callback) {
       break;
   }
 
-  console.log('set reg', reg);
-
   self._read16Bits(reg, function(err, color) {
 
     if (!self.failCallback(err, callback)) {
@@ -205,20 +223,128 @@ RGB.prototype.readColor = function(color, callback) {
   });
 };
 
+RGB.prototype.calculateColorTemperature = function(callback) {
+  var self = this;
+
+  self.getRawData(function(err, colors) {
+    if (!self.failCallback(err, callback)) {
+      /* 1. Map RGB values to their XYZ counterparts.    */
+      /* Based on 6500K fluorescent, 3000K fluorescent   */
+      /* and 60W incandescent values for a wide range.   */
+      /* Note: Y = Illuminance or lux                    */
+      var r = colors.red;
+      var g = colors.green;
+      var b = colors.blue;
+
+      var X = (-0.14282 * r) + (1.54924 * g) + (-0.95641 * b);
+      var Y = (-0.32466 * r) + (1.57837 * g) + (-0.73191 * b);
+      var Z = (-0.68202 * r) + (0.77073 * g) + ( 0.56332 * b);
+
+      /* 2. Calculate the chromaticity co-ordinates      */
+      var xc = (X) / (X + Y + Z);
+      var yc = (Y) / (X + Y + Z);
+
+      /* 3. Use McCamy's formula to determine the CCT    */
+      var n = (xc - 0.3320) / (0.1858 - yc);
+
+      /* Calculate the final CCT */
+      var cct = (449.0 * Math.pow(n, 3)) + (3525.0 * Math.pow(n, 2)) + (6823.3 * n) + 5520.33;
+
+      /* Return the results in degrees Kelvin */
+      if (callback) {
+        callback(null, cct);
+      }
+    }
+  });
+};
+
+RGB.prototype.calculateLux = function(callback) {
+  var self = this;
+
+  self.getRawData(function(err, colors) {
+    if (!self.failCallback(err, callback)) {
+      /* This only uses RGB ... how can we integrate clear or calculate lux */
+      /* based exclusively on clear since this might be more reliable?      */
+      var illuminance = (-0.32466 * colors.red) + (1.57837 * colors.green) + (-0.73191 * colors.blue);
+      if (callback) {
+        callback(null, illuminance);
+      }
+
+    }
+  });
+};
+
+RGB.prototype.setInterrupt = function(enable, callback) {
+  var self = this;
+
+  self._read8Bits(TCS34725_ENABLE, function(err, val) {
+    if (!self.failCallback(err, callback)) {
+      if (enable) {
+        val = val.readUInt8(0) | TCS34725_ENABLE_AIEN;
+
+        self.irq.on('fall', function() {
+          self._clearInterrupt(function() {
+            self.emit('interrupt');
+          });
+        });
+      }
+      else {
+        val &= ~TCS34725_ENABLE_AIEN
+
+        self.irq.removeAllListeners();
+      }
+      self._write8Bits(TCS34725_ENABLE, val, callback);
+    }
+  })
+};
+
+RGB.prototype._clearInterrupt = function(callback) {
+  var self = this;
+
+  self.i2c.send(new Buffer([0x66]), callback);
+};
+
+RGB.prototype.setIntLimits = function(low, high, callback) {
+  var self = this;
+
+  self.setInterrupt(true, function(err) {
+    if (!self.failCallback(err, callback)) {
+      self._setLowIntLimit(low, function(err) {
+        if (!self.failCallback(err, callback)) {
+          self._setHighIntLimit(high, callback);
+        }
+      });
+    }
+  });
+};
+
+RGB.prototype._setLowIntLimit = function(val, callback) {
+  var self = this;
+
+  self._write8Bits(TCS34725_AILTL, val & 0xFF, function(err) {
+    if (!self.failCallback(err, callback)) {
+      self._write8Bits(TCS34725_AILTH, val >> 8, callback);
+    }
+  });
+};
+
+RGB.prototype._setHighIntLimit = function(val, callback) {
+  var self = this;
+
+  self._write8Bits(TCS34725_AIHTL, val & 0xFF, function(err) {
+    if (!self.failCallback(err, callback)) {
+      self._write8Bits(TCS34725_AIHTH, val >> 8, callback);
+    }
+  });
+};
+
 RGB.prototype.setLED = function(on, callback) {
-  this.hardware.digital[0].output(on);
+  this.hardware.digital[1].output(on);
 
   if (callback) {
     callback();
   }
 };
 
-var tessel = require('tessel');
-var r = use(tessel.port.A);
-r.on('ready', function() {
-  
-
-  r.getRGBC(function(err, rgbc) {
-    console.log('got this!', rgbc);
-  })
-})
+module.exports.use = use;
+module.exports.RGB = RGB;
